7a638ab5b5069dd0421e73df4b33f8af56c1d9de
Zhanar <omarovazhk@gmail.com>
2025-11-02 23:13:22 +0500

    Создала маршруты к шаблонам

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from webapp.views import task_list_view
+from webapp.views import task_list_view, task_create_view, task_delete_view
+
 urlpatterns = [
-    path('', task_list_view),
+    path('', task_list_view, name='task_list'),
+    path('create/', task_create_view, name='task_create'),
+    path('delete/<int:pk>/', task_delete_view, name='task_delete'),
 ]
